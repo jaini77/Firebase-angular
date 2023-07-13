4b4ca41c542edd94dd75a623ca867476e06c366e
= <=>
2023-07-13 18:27:05 +0530

    save

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-app-7a7a8',
+    appId: '1:552224815485:web:cefd0decd00c5c58bd3414',
+    databaseURL: 'https://ang-app-7a7a8-default-rtdb.firebaseio.com',
+    storageBucket: 'ang-app-7a7a8.appspot.com',
+    apiKey: 'AIzaSyC-kfT_oGXntWObR0vT0NWGLZDH6jkAQzg',
+    authDomain: 'ang-app-7a7a8.firebaseapp.com',
+    messagingSenderId: '552224815485',
+    measurementId: 'G-2MXK1STHST',
+  },
+};
